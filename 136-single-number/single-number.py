bd6e27a71d8e5db8401faffc3638906dd364368b
Zia Ur Rehman <ziaulrehman6349@gmail.com>
2025-06-18 22:09:39 +0500

    Time: 7 ms (33.84%) | Memory: 19.8 MB (23.13%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,6 +1,21 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        k = Counter(nums)   # Count Numbers
-        for i in k:
-            if k[i] == 1:
-                return i 
+        dic = {}    # {4:1 , 1:2 , 2:2 }
+        for i in nums:  #nums = [4,1,2,1,2]
+
+            if i in dic:
+                dic[i] += 1
+            else:
+                dic[i] = 1
+                
+        for i in dic:
+            if dic[i] == 1:
+                return i
+
+                                # (Alternative Method)
+
+
+#         k = Counter(nums)   
+#         for i in k:         
+#             if k[i] == 1:
+#                 return i 
