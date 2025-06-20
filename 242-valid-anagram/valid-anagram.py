9ae28357eab7667974e6d7986cb7705b8ce450e7
Zia Ur Rehman <ziaulrehman6349@gmail.com>
2025-06-20 18:40:36 +0500

    Time: 16 ms (24.41%) | Memory: 18.6 MB (11.31%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,47 @@
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         s = list(s)
+#         t = list(t) 
+#         a = s.sort()
+#         b = t.sort()
+#         if s == t:
+#             return True
+#         else:
+#             return False
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        a = list(s)
+        b = list(t)
+        a.sort()
+        b.sort()
+        if a==b:
+            return True
+        else:
+            return False
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        a = list(s)
+        b = list(t)
+        a.sort()
+        b.sort()
+        if a==b:
+            return True
+        else:
+            return False
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        a = list(s)
+        b = list(t)
+        a.sort()
+        b.sort()
+        if a==b:
+            return True
+        else:
+            return False
+
+
+
