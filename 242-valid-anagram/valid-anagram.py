c7318ea350c53715539934c23a75bb63c7372749
Zia Ur Rehman <ziaulrehman6349@gmail.com>
2025-06-20 18:43:08 +0500

    Time: 19 ms (15.84%) | Memory: 18.5 MB (19.19%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -2,8 +2,8 @@
 #     def isAnagram(self, s: str, t: str) -> bool:
 #         s = list(s)
 #         t = list(t) 
-#         a = s.sort()
-#         b = t.sort()
+#         a = s.sort()       point to be noted       it should be      s.sort() 
+#         b = t.sort()       point to be noted       it shoudl be      t.sort()
 #         if s == t:
 #             return True
 #         else:
@@ -20,28 +20,6 @@ class Solution:
         else:
             return False
 
-
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        a = list(s)
-        b = list(t)
-        a.sort()
-        b.sort()
-        if a==b:
-            return True
-        else:
-            return False
-
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        a = list(s)
-        b = list(t)
-        a.sort()
-        b.sort()
-        if a==b:
-            return True
-        else:
-            return False
 
 
 
