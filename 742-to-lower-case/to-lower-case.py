2f35e9711d0337e61755318d1014cac4edac167f
Zia Ur Rehman <ziaulrehman6349@gmail.com>
2025-06-29 14:47:45 +0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (70.59%) - LeetSync

--- 742-to-lower-case/to-lower-case.py
+++ 742-to-lower-case/to-lower-case.py
@@ -1,4 +1,15 @@
 class Solution:
     def toLowerCase(self, s: str) -> str:
-        return s.lower()
+      #  return s.lower()
+
+
+        result = ""
+
+        for char in s:
+            if char.isupper():
+                char = char.lower()
+                result += char
+            else:
+                result += char
+        return result
         
