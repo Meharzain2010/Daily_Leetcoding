fa33a0001e68394e610c39e8ef0e3163b6f290df
Zia Ur Rehman <ziaulrehman6349@gmail.com>
2025-07-05 14:34:49 +0500

    Time: 0 ms (100.00%) | Memory: 17.4 MB (99.45%) - LeetSync

--- 1505-create-target-array-in-the-given-order/create-target-array-in-the-given-order.py
+++ 1505-create-target-array-in-the-given-order/create-target-array-in-the-given-order.py
@@ -1,8 +1,9 @@
 class Solution:
     def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
-                                #nums = [0,1,2,3,4], index = [0,1,2,2,1]
+
+                                # nums = [0,1,2,3,4,5], index = [0,1,2,2,1,4]
         target_array = []
-        for i in range(0,len(nums)): 
+        for i in range(len(nums)): 
             target_array.insert(index[i],nums[i])
         return target_array
 
