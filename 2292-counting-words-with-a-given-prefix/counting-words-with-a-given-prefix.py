e1f9b27f333a64d714243d1a31a5a9fb46656d3b
Zia Ur Rehman <ziaulrehman6349@gmail.com>
2025-09-12 18:18:15 +0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (67.16%) - LeetSync

--- 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.py
+++ 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.py
@@ -7,4 +7,24 @@ class Solution:
             if word.startswith(pref):
                 output.append(word)
         return len(output)
-            
+        
+class Solution:
+    def prefixCount(self, words: List[str], pref: str) -> int:
+        #words = ["pay","attention","practice","attend"], 
+        #pref = "at"
+        output = 0
+        for word in words:
+            if word.startswith(pref):
+                output  += 1
+        return output
+
+class Solution:
+    def prefixCount(self, words: List[str], pref: str) -> int:
+        #words = ["pay","attention","practice","attend"], 
+        #pref = "at"
+        count = 0
+        pref_length = len(pref)
+        for word in words:
+            if word[:pref_length] == pref:
+                count +=1
+        return count
