0241d6a8b37e4092b09010b566fc40465c742910
Zia Ur Rehman <ziaulrehman6349@gmail.com>
2025-09-12 19:31:32 +0500

    Time: 1 ms (53.29%) | Memory: 17.9 MB (18.23%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,21 +1,21 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        seen = []        # n = 19
+        # seen = set()        # n = 19
         
+        # while n != 1 and n not in seen:
+        #     seen.add(n)
+        #     sum_sqr_digits = 0
+        #     while n > 0:
+        #         sum_sqr_digits += (n % 10) ** 2 # 19 / 10 = 1
+        #         n = n // 10 #0
+        #     n = sum_sqr_digits
+        
+        # return n == 1
+        
+# class Solution(object):
+    # def isHappy(self, n):
+        seen = []
         while n != 1 and n not in seen:
             seen.append(n)
-            sum_sqr_digits = 0
-            while n > 0:
-                sum_sqr_digits += (n % 10) ** 2 # 19 / 10 = 1
-                n = n // 10 #0
-            n = sum_sqr_digits
-        
-        return n == 1
-        
-class Solution(object):
-    def isHappy(self, n):
-        seen = set()
-        while n != 1 and n not in seen:
-            seen.add(n)
-            n = sum(int(digit) * int(digit) for digit in str(n))
+            n = sum(int(digit) * int(digit) for digit in str(n))   
         return n == 1
