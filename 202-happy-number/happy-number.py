538c7f73d929e6eed10fce6fe271e601548d90a5
Zia Ur Rehman <ziaulrehman6349@gmail.com>
2025-09-12 19:21:38 +0500

    Time: 3 ms (39.73%) | Memory: 17.7 MB (61.21%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -11,3 +11,11 @@ class Solution:
             n = sum_sqr_digits
         
         return n == 1
+        
+class Solution(object):
+    def isHappy(self, n):
+        seen = set()
+        while n != 1 and n not in seen:
+            seen.add(n)
+            n = sum(int(digit) * int(digit) for digit in str(n))
+        return n == 1
