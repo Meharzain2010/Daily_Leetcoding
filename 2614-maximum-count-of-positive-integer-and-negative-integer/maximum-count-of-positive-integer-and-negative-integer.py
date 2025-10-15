2fa1f7c077fef0dfa9b099c27151924b4d83af8b
Zia Ur Rehman <ziaulrehman6349@gmail.com>
2025-10-15 22:49:02 +0500

    Time: 0 ms (100.00%) | Memory: 18.1 MB (43.16%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.py
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.py
@@ -8,10 +8,10 @@ class Solution:
                 neg += 1
             elif num > 0:
                 pos += 1
-        # return max(pos,neg)
-        if pos >= neg:
-            return pos
-        elif pos < neg:
-            return neg
+        return max(pos,neg)
+        # if pos >= neg:
+        #     return pos
+        # elif pos < neg:
+        #     return neg
         # elif pos == neg:
         #     return pos
