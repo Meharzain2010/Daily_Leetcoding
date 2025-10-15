958aeea3074f726cdd1857bbe1380c3b89f86092
Zia Ur Rehman <ziaulrehman6349@gmail.com>
2025-10-15 23:04:56 +0500

    Time: 2 ms (32.10%) | Memory: 17.9 MB (92.92%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.py
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.py
@@ -3,15 +3,30 @@ class Solution:
         pos = 0 
         neg = 0
         
-        for num in nums:
+        for num in nums: 
             if num < 0:
                 neg += 1
             elif num > 0:
                 pos += 1
         return max(pos,neg)
+        
+        
         # if pos >= neg:
         #     return pos
         # elif pos < neg:
         #     return neg
         # elif pos == neg:
         #     return pos
+        
+        
+        
+        
+        
+        
+        
+        
+        # pos = [1,1,1,2,3]
+        # neg = [-2,-1,-1]
+        # # nums = [-2,-1,-1,0,1,1,1,2,3]
+        # pos_len = len(pos)
+        # neg_len = len(neg)
