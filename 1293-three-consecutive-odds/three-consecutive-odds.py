017ce34e53687c47f5a7792bbf47442705aadbc5
Zia Ur Rehman <ziaulrehman6349@gmail.com>
2025-10-23 22:22:36 +0500

    Time: 0 ms (100.00%) | Memory: 18 MB (40.64%) - LeetSync

--- 1293-three-consecutive-odds/three-consecutive-odds.py
+++ 1293-three-consecutive-odds/three-consecutive-odds.py
@@ -1,17 +1,11 @@
 class Solution:
     def threeConsecutiveOdds(self, arr: List[int]) -> bool:
-        """
-        step 1: jo number 2 pr divide kr raha ho us k 
-        step 2:
-        step 3:
-        """
+
         if len(arr) < 3:
             return False 
             
-        for i in range(len(arr) - 2):    # [1,2,34,3,4,5,7,23,12]
+        for i in range(len(arr) - 2): 
             if (arr[i] % 2 != 0) and (arr[i+1] % 2 != 0) and (arr[i+2] % 2 != 0):
                 return True
         return False
         
-                        
-                
