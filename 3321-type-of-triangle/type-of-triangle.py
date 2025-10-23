0f9bd92919e98558ffa88744cd5353a399bb63f2
Zia Ur Rehman <ziaulrehman6349@gmail.com>
2025-10-23 21:12:12 +0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (50.86%) - LeetSync

--- 3321-type-of-triangle/type-of-triangle.py
+++ 3321-type-of-triangle/type-of-triangle.py
@@ -27,19 +27,26 @@ class Solution:
         all three cases, therefore, it can form a triangle.
         As all the sides are of different lengths, it will form a scalene triangle.
         """
-        #nums = [3,4,5]
-        if nums[0] + nums[1] > nums[2] and nums[1] + nums[2] > nums[0] and nums[2] + nums[0] > nums[1]:
-            if nums[0] == nums[1] == nums[2]:
-                return "equilateral"
-            elif nums[0] == nums[1] or nums[1] == nums[2] or nums [0] == nums[2]:
-                return "isosceles"
-            elif nums[0] != nums[1] != nums[2]:
-                return "scalene"     
-        else:
+        # if nums[0] + nums[1] > nums[2] and nums[1] + nums[2] > nums[0] and nums[2] + nums[0] > nums[1]:
+        #     if nums[0] == nums[1] == nums[2]:
+        #         return "equilateral"
+        #     elif nums[0] == nums[1] or nums[1] == nums[2] or nums [0] == nums[2]:
+        #         return "isosceles"
+        #     elif nums[0] != nums[1] != nums[2]:
+        #         return "scalene"     
+        # else:
+        #     return "none"
+            
+            
+        if not (nums[0] + nums[1] > nums[2] and nums[1] + nums[2] > nums[0] and nums[2] + nums[0] > nums[1]):
             return "none"
-            
-            
-            
+        
+        if nums[0] == nums[1] == nums[2]:
+            return "equilateral"
+        elif nums[0] == nums[1] or nums[1] == nums[2] or nums [0] == nums[2]:
+            return "isosceles"
+        else:
+            return "scalene"     
 
 
             
